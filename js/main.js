4798fe770b70dfdcecc8de11a21ae3d468c7b274
iam-developer <turgunbaaev@gmail.com>
2021-01-21 01:02:17 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+
+// Форма обратной связи. Чекбокс:
+$(document).ready(function() {
+    $('.galochka--ramka').on('click', function() {
+        $(this).find('.check--img').toggleClass('selected')
+    });
+
+// Страница "Оформление заказа"
+    // $('.oformit--btn').on('click', function() {
+        $(".oformit--btn").click(function() {
+            var $emptyFields = $('.oform--input').filter(function() {
+                return $.trim(this.value) === "";
+            });
+        
+            if (!$emptyFields.length) {
+                $('.oformlenie--bloock').css('display', 'none');
+                $('.oform--succes').css('display', 'block');
+            }
+        });
+    // });
+});
+
 // Бургер меню:
 let menu = document.querySelector('.fixed--menu');
 let closeBtn = document.querySelector('.close--menu');
@@ -5,7 +27,9 @@ let openBtn = document.querySelector('.burger--menu');
 
 openBtn.onclick = function() {
     menu.classList.add('active');
+    closeBtn.classList.add('active');
 }
 closeBtn.onclick = function() {
     menu.classList.remove('active');
+    closeBtn.classList.remove('active');
 }
